dfbb9d8892d528c09ac5e3aff4f2445363276a57
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2025-02-08 23:04:55 -0300

    Update Catalogo.jsx

--- src/assets/components/Catalogo.jsx
+++ src/assets/components/Catalogo.jsx
@@ -1,17 +1,19 @@
-import { Burguer } from "./Burguer"
+import { Burguer } from "./Burguer";
 
 /* eslint-disable react/prop-types */
 export const Catalago = ({ burguers }) => {
     return (
         <section>
-            <div className="flex justify-center">
-                {burguers.map((burguer) => <Burguer
-                    key={burguer.id}
-                    nome={burguer.nome}
-                    descricao={burguer.descricao}
-                    price = {`R$ ${burguer.price}`} />
-                )}
+            <div className="flex flex-col sm:flex-row justify-center">
+                {burguers.map((burguer) => (
+                    <Burguer
+                        key={burguer.id}
+                        nome={burguer.nome}
+                        descricao={burguer.descricao}
+                        price={`R$ ${burguer.price}`}
+                    />
+                ))}
             </div>
         </section>
-    )
-}
+    );
+};
