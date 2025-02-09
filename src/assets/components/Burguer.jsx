fab0f8652069c9349efeb0a31fa08047821604d9
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2025-02-08 23:06:38 -0300

    Update Burguer.jsx

--- src/assets/components/Burguer.jsx
+++ src/assets/components/Burguer.jsx
@@ -1,14 +1,16 @@
-
 export const Burguer = (burguers) => {
-    return (
-        <div className="bg-gray-900/30  m-10 p-10 rounded-3xl h-120 w-120 flex flex-col justify-between ">
-            <p className="text-white text-3xl p-2">{burguers.nome}</p>
-            <p className="text-white text-2xl">{burguers.descricao}</p>
-            <p className="text-white text-2xl">{burguers.price}</p>
-            <div>
-                <img className="h-76 w-250 rounded-2xl" src= "/img/burger.jpg" alt="cheese" />
-            </div>
-
-        </div>
-    )
-}
+  return (
+    <div className="bg-gray-900/30 m-4 p-4 sm:m-10 sm:p-10 rounded-3xl flex flex-col justify-between max-w-[300px]">
+      <p className="text-white text-xl sm:text-3xl p-2">{burguers.nome}</p>
+      <p className="text-white text-lg sm:text-2xl">{burguers.descricao}</p>
+      <p className="text-white text-lg sm:text-2xl">{burguers.price}</p>
+      <div>
+        <img
+          className="w-full h-auto rounded-2xl"
+          src="/img/burger.jpg"
+          alt="cheese"
+        />
+      </div>
+    </div>
+  );
+};
