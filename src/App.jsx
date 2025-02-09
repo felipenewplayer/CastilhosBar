4b376c0e9c13010fa227cfa36ea85f480023ade9
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2025-02-08 23:57:12 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,37 @@ function App() {
     {
       id: 1,
       nome: "HamburguerX",
-      descricao: "4 queijos, salada, cheedar, bacon",
+      descricao: "Queijo mussarela, salada, cheedar, bacon",
        price:29.99
     },
     {
       id: 2,
       nome: "HamburguerX",
-      descricao: "4 queijos, salada, cheedar, bacon",
+      descricao: "Queijo mussarela, salada, cheedar, bacon",
       price:29.99
     },
     {
       id: 3,
       nome: "HamburguerX",
-      descricao: "4 queijos, salada, cheedar, bacon",
+      descricao: "Queijo mussarela, salada, cheedar, bacon",
       price:29.99
     },
     {
       id: 4,
       nome: "HamburguerX",
-      descricao: "4 queijos, salada, cheedar, bacon",
+      descricao: "Queijo mussarela, salada, cheedar, bacon",
       price: 29.99
     },
     {
       id: 5,
       nome: "HamburguerX",
-      descricao: "4 queijos, salada, cheedar, bacon",
+      descricao: "Queijo mussarela, salada, cheedar, bacon",
       price:29.99
     },
     {
       id: 6,
       nome: "HamburguerX",
-      descricao: "4 queijos, salada, cheedar, bacon",
+      descricao: "Queijo mussarela, salada, cheedar, bacon",
       price:29.99
     },
   ])
