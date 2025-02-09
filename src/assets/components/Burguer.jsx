ca1d2859104fac744762913b2b399dbe8bb115ae
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2025-02-09 12:27:44 -0300

    arrumando

--- src/assets/components/Burguer.jsx
+++ src/assets/components/Burguer.jsx
@@ -1,33 +1,15 @@
-<<<<<<< HEAD
 /* eslint-disable react/prop-types */
-export const Burguer = ({ nome, descricao, price }) => {
-    return (
-      <div className="bg-gray-900/30 m-4 p-4 sm:m-10 sm:p-10 rounded-3xl flex flex-col justify-between max-w-xs w-full">
-        <p className="text-white text-xl sm:text-3xl p-2">{nome}</p>
-        <p className="text-white text-base sm:text-2xl">{descricao}</p>
-        <p className="text-white text-base sm:text-2xl">{price}</p>
-        <div>
-          <img className="w-full h-auto rounded-2xl" src="/img/burger.jpg" alt="cheese" />
-        </div>
-      </div>
-    );
-  };
-  
-=======
-export const Burguer = (burguers) => {
-  return (
-    <div className="bg-gray-900/30 m-4 p-4 sm:m-10 sm:p-10 rounded-3xl flex flex-col justify-between max-w-[300px]">
-      <p className="text-white text-xl sm:text-3xl p-2">{burguers.nome}</p>
-      <p className="text-white text-lg sm:text-2xl">{burguers.descricao}</p>
-      <p className="text-white text-lg sm:text-2xl">{burguers.price}</p>
-      <div>
-        <img
-          className="w-full h-auto rounded-2xl"
-          src="/img/burger.jpg"
-          alt="cheese"
-        />
-      </div>
+
+export const Burguer = ({ nome, descricao, price }) => (
+  <div className="bg-gray-900/30 m-4 p-4 sm:m-10 sm:p-10 rounded-3xl flex flex-col justify-between max-w-xs w-full">
+    <p className="text-white text-xl sm:text-3xl p-2">{nome}</p>
+    <p className="text-white text-base sm:text-2xl">{descricao}</p>
+    <p className="text-white text-base sm:text-2xl">{price}</p>
+    <div>
+      <img className="w-full h-auto rounded-2xl" src="/img/burger.jpg" alt="cheese" />
     </div>
-  );
-};
->>>>>>> 4b376c0e9c13010fa227cfa36ea85f480023ade9
+  </div>
+);
+  
+
+
--- src/assets/components/Catalogo.jsx
+++ src/assets/components/Catalogo.jsx
@@ -2,7 +2,6 @@ import { Burguer } from "./Burguer";
 
 /* eslint-disable react/prop-types */
 export const Catalago = ({ burguers }) => {
-<<<<<<< HEAD
   return (
     <section className="p-4">
       <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center gap-4">
@@ -16,21 +15,4 @@ export const Catalago = ({ burguers }) => {
         ))}
       </div>
     </section>
-  );
-=======
-    return (
-        <section>
-            <div className="flex flex-col sm:flex-row justify-center">
-                {burguers.map((burguer) => (
-                    <Burguer
-                        key={burguer.id}
-                        nome={burguer.nome}
-                        descricao={burguer.descricao}
-                        price={`R$ ${burguer.price}`}
-                    />
-                ))}
-            </div>
-        </section>
-    );
->>>>>>> 4b376c0e9c13010fa227cfa36ea85f480023ade9
-};
+  );}
