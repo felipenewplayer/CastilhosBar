22e0fc8e3c0a6a0653ad6251b32b027d991dc8bb
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2025-02-08 23:00:15 -0300

    Update Banner.jsx

--- src/assets/components/Banner.jsx
+++ src/assets/components/Banner.jsx
@@ -1,9 +1,9 @@
 import { Login } from "./pages/Login";
 
 export const Banner = () => {
-    return (
-        <div className="bg-black p-4 sm:p-6 md:p-10 lg:p-15 xl:p-20 flex flex-col lg:flex-row xl:flex-row justify-between items-center border-b border-gray-500/50 mb-10">
-            <div className="flex flex-col lg:flex-row xl:flex-row sm:flex-grow justify-between sm:w-auto w-full text-center">
+    return(
+        <div className="bg-black p-4 sm:p-6 md:p-10 lg:p-15 flex flex-col sm:flex-row justify-between items-center border-b border-gray-500/50 mb-10">
+            <div className="flex flex-row sm:flex-grow justify-between sm:w-auto w-full text-center">
                 <a className="text-white text-xl sm:text-3xl md:text-3xl" href="">Menu</a>
                 <a className="text-white text-xl sm:text-3xl md:text-3xl" href="">Sobre</a>
                 <a className="text-white text-3xl sm:text-5xl md:text-5xl" href="">Castilhos Bar</a>
@@ -13,5 +13,5 @@ export const Banner = () => {
                 <Login />
             </div>
         </div>
-    );
+    )
 }
