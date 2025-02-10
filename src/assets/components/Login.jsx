34c99615196d959fee8a2ad3c71ca76161e06efd
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2025-02-10 10:19:38 -0300

    arrumando

--- src/assets/components/Login.jsx
+++ src/assets/components/Login.jsx
@@ -1,12 +1,14 @@
+import { useNavigate } from "react-router-dom"
 
 export const Login = () => {
-
+    const navigate = useNavigate();
 
     return (
         <button
-            className="bg-purple-100 h-10 rounded-md hover:scale-120 transition duration-700 ease-linear hover:bg-orange-600  "
+            className="bg-purple-100 h-10 rounded-md hover:scale-120 transition duration-700 ease-linear hover:bg-orange-600 "
+            onClick={() => navigate('/login')}
         >
-            <a className="text-4xl text-black p-10 pt-10 hover:text-white" href="/login">Login</a>
+            <span className="text-4xl text-black p-10 pt-10 hover:text-white" >Login</span>
         </button>
     )
 }   
