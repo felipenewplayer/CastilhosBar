aefffc8d3e7f09944e66014874d4508ccae353f8
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2025-02-10 16:32:20 -0300

    Update Paragraph.jsx

--- src/assets/components/Paragraph.jsx
+++ src/assets/components/Paragraph.jsx
@@ -2,7 +2,7 @@
 export const Paragraph = ({children, rest}) =>{
     return(
        <p
-       className="text-white text-base sm:text-2xl p-4 "
+       className="text-white text-base sm:text-2xl p-3 "
        {...rest}
          >{children}</p>
     )
